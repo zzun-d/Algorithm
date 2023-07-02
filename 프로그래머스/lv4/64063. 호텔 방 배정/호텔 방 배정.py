2b5694732ef728877c497f547cc6ea3c8036da06
zzun-d <102124509+zzun-d@users.noreply.github.com>
2023-07-02 15:16:03 +0900

    [level 4] Title: 호텔 방 배정, Time: 437.06 ms, Memory: 52.6 MB -BaekjoonHub

--- 프로그래머스/lv4/64063. 호텔 방 배정/호텔 방 배정.py
+++ 프로그래머스/lv4/64063. 호텔 방 배정/호텔 방 배정.py
@@ -1,18 +1,13 @@
-import sys
-
-sys.setrecursionlimit(1000000)
-
 def solution(k, room_number):
-    def find_parent(x):
-        if not table.get(x):
-            table[x] = x + 1
-            return x
-        else:
-            table[x] = find_parent(table[x])
-            return table[x]
-    result = []
-    table = dict()
-    for room in room_number:
-        result.append(find_parent(room))
-
-    return result
+    room_dic = {}
+    ret = []
+    for i in room_number:
+        n = i
+        visit = [n]
+        while n in room_dic:
+            n = room_dic[n]
+            visit.append(n)
+        ret.append(n)
+        for j in visit:
+            room_dic[j] = n+1
+    return ret
