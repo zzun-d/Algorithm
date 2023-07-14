768e32fd147ca2ad3f27c6f11efafdba2773f9ae
zzun-d <102124509+zzun-d@users.noreply.github.com>
2023-07-14 12:00:05 +0900

    [Silver I] Title: 신입 사원, Time: 3032 ms, Memory: 58416 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.py
+++ 백준/Silver/1946. 신입 사원/신입 사원.py
@@ -10,8 +10,7 @@ for _ in range(T):
     lst = [tuple(map(int, input().split())) for _ in range(N)]
     lst.sort()
     now_score = lst[0][1]
-    for i, scores in enumerate(lst[1:], start=1):
-        score = scores[1]
+    for _, score in lst[1:]:
         if score < now_score:
             ans += 1
             now_score = score
