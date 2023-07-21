b69a3756c780de9eddbcb542ff4adb386a3ee9a0
zzun-d <102124509+zzun-d@users.noreply.github.com>
2023-07-21 22:03:18 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.01 ms, Memory: 3.64 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string phone_number) {
+    string answer = "";
+    for (int i=4; i<phone_number.size(); i++) {
+        answer += "*";
+    }
+    for (int i= phone_number.size() - 4; i < phone_number.size(); i++) {
+        answer += phone_number[i];
+    }
+     
+    return answer;
+}
