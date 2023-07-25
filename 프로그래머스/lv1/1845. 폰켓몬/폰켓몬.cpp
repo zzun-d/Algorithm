a47e772502a3bcf08c7ff3424f6163fc0f17fe3a
zzun-d <102124509+zzun-d@users.noreply.github.com>
2023-07-25 16:24:04 +0900

    [level 1] Title: 폰켓몬, Time: 0.22 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.cpp
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.cpp
@@ -1,18 +1,17 @@
 #include <vector>
 #include <algorithm>
-
+#include <set>
 using namespace std;
 
 int solution(vector<int> nums)
 {
-    int answer = 0;
-    int l = nums.size();
-    sort(nums.begin(), nums.end());
-    nums.erase(unique(nums.begin(), nums.end()), nums.end());
-    if (nums.size() > l/2) {
-        answer = l/2;
-    } else {
-        answer = nums.size();
-    }
-    return answer;
+    set<int> answer(nums.begin(), nums.end());
+    // sort(nums.begin(), nums.end());
+    // nums.erase(unique(nums.begin(), nums.end()), nums.end());
+    // if (nums.size() > l/2) {
+    //     answer = l/2;
+    // } else {
+    //     answer = nums.size();
+    // }
+    return min(answer.size(), nums.size()/2);
 }
